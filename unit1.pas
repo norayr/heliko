5876c603a1c63c11983b578e2bb547fa4f9b8096
Norayr Chilingarian <norayr@arnet.am>
2025-03-13 17:09:55 +0400

    compiles.

--- unit1.pas
+++ unit1.pas
@@ -1,4 +1,4 @@
-unit Unit1;
+                                                            unit Unit1;
 
 {$mode objfpc}{$H+}
 
@@ -6,25 +6,21 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs,
-  SynEdit, SynEditMiscClasses, LCLType, Process, LCLIntf;
+  SynEdit, SynEditTypes, SynEditMiscClasses, LCLType, Process, StrUtils;
 
 type
   TForm1 = class(TForm)
     SynEdit1: TSynEdit;
     procedure FormCreate(Sender: TObject);
+    procedure SynEdit1SpecialLineMarkup(Sender: TObject; Line: integer;
+      var Special: boolean; Markup: TSynSelectedColor);
     procedure SynEdit1MouseMove(Sender: TObject; Shift: TShiftState; X, Y: Integer);
     procedure SynEdit1MouseLeave(Sender: TObject);
     procedure SynEdit1Click(Sender: TObject);
   private
-    CurrentCommand: string;
-    CmdStart, CmdEnd, CmdLine: Integer;
-    procedure ExecuteUnixCommand(const Cmd: string);
+    CmdLine, CmdStart, CmdEnd: Integer;
     function GetCommandUnderMouse(X, Y: Integer): string;
-    procedure SynEdit1SpecialLineMarkup(Sender: TObject; Line: integer;
-  var Special: boolean; Markup: TSynSelectedColor);
-
-  public
-
+    procedure ExecuteUnixCommand(const Cmd: string);
   end;
 
 var
@@ -36,16 +32,50 @@ implementation
 
 procedure TForm1.FormCreate(Sender: TObject);
 begin
-  SynEdit1.Text := 'Type commands enclosed in semicolons: for example ;ls -la; or ;uname -a;';
-   SynEdit1.OnSpecialLineMarkup := @SynEdit1SpecialLineMarkup;
-   CurrentCommand := '';
-   CmdStart := -1;
-   CmdEnd := -1;
-   CmdLine := -1;
+  SynEdit1.Text := 'Try Unix commands enclosed in ";" like ;ls -la; or ;uname -a;.';
+  CmdLine := -1;
+  CmdStart := -1;
+  CmdEnd := -1;
 end;
 
-procedure TForm1.SynEdit1SpecialLineMarkup(Sender: TObject; Line: integer;
-  var Special: boolean; Markup: TSynSelectedColor);
+function TForm1.GetCommandUnderMouse(X, Y: Integer): string;
+var
+  Pt: TPoint;
+  LineText: string;
+  CharIdx: Integer;
+begin
+  Pt := SynEdit1.PixelsToRowColumn(Point(X, Y));
+  CmdLine := Pt.Y - 1;
+
+  Result := '';
+  CmdStart := -1;
+  CmdEnd := -1;
+
+  if (CmdLine < 0) or (CmdLine >= SynEdit1.Lines.Count) then Exit;
+
+  LineText := SynEdit1.Lines[CmdLine];
+  CharIdx := Pt.X;
+
+  if (CharIdx < 1) or (CharIdx > Length(LineText)) then Exit;
+
+  CmdStart := RPosEx(';', LineText, CharIdx);
+  CmdEnd := PosEx(';', LineText, CharIdx);
+
+  if (CmdStart > 0) and (CmdEnd > CmdStart) then
+  begin
+    Result := Trim(Copy(LineText, CmdStart + 1, CmdEnd - CmdStart - 1));
+    SynEdit1.InvalidateLine(CmdLine);
+  end
+  else
+  begin
+    CmdStart := -1;
+    CmdEnd := -1;
+    CmdLine := -1;
+  end;
+end;
+
+procedure TForm1.SynEdit1SpecialLineMarkup(Sender: TObject; Line: Integer;
+  var Special: Boolean; Markup: TSynSelectedColor);
 begin
   if (Line = CmdLine) and (CmdStart > 0) and (CmdEnd > CmdStart) then
   begin
@@ -56,60 +86,18 @@ begin
   end;
 end;
 
-
-function TForm1.GetCommandUnderMouse(X, Y: Integer): string;
-var
-  Pt: TPoint;
-  LineText: string;
-  LineIdx, CharIdx, leftDelim, rightDelim: Integer;
-begin
-  Result := '';
-  Pt := SynEdit1.PixelsToRowColumn(Point(X, Y));
-  LineIdx := Pt.Y - 1;  // Lazarus lines are zero-based
-
-  if (LineIdx < 0) or (LineIdx >= SynEdit1.Lines.Count) then Exit;
-
-  LineText := SynEdit1.Lines[LineIdx];
-  CharIdx := Pt.X;
-
-  if (CharIdx < 1) or (CharIdx > Length(LineText)) then Exit;
-
-  leftDelim := CharIdx;
-  while (leftDelim >= 1) and (LineText[leftDelim] <> ';') do Dec(leftDelim);
-  rightDelim := CharIdx;
-  while (rightDelim <= Length(LineText)) and (LineText[rightDelim] <> ';') do Inc(rightDelim);
-
-  if (leftDelim >= 1) and (rightDelim <= Length(LineText)) and (leftDelim < rightDelim) then
-  begin
-    Result := Trim(Copy(LineText, leftDelim + 1, rightDelim - leftDelim - 1));
-    CmdStart := leftDelim;
-    CmdEnd := rightDelim;
-    CmdLine := LineIdx;
-  end
-  else
-  begin
-    CmdStart := -1;
-    CmdEnd := -1;
-    CmdLine := -1;
-  end;
-end;
-
 procedure TForm1.SynEdit1MouseMove(Sender: TObject; Shift: TShiftState; X, Y: Integer);
 var
-  Cmd: string;
   OldLine: Integer;
+  Cmd: string;
 begin
   OldLine := CmdLine;
   Cmd := GetCommandUnderMouse(X, Y);
   if Cmd <> '' then
     SynEdit1.Cursor := crHandPoint
   else
-  begin
     SynEdit1.Cursor := crDefault;
-    CmdLine := -1;
-  end;
 
-  // Redraw only if hover line changed
   if OldLine <> CmdLine then
   begin
     if OldLine >= 0 then SynEdit1.InvalidateLine(OldLine);
@@ -120,22 +108,24 @@ end;
 procedure TForm1.SynEdit1MouseLeave(Sender: TObject);
 begin
   SynEdit1.Cursor := crDefault;
-  CurrentCommand := '';
-  CmdStart := -1;
-  CmdEnd := -1;
-  CmdLine := -1;
+  if CmdLine >= 0 then
+  begin
+    SynEdit1.InvalidateLine(CmdLine);
+    CmdLine := -1;
+  end;
 end;
 
 procedure TForm1.SynEdit1Click(Sender: TObject);
 var
-  Cmd: string;
   MousePos: TPoint;
+  Cmd: string;
 begin
   MousePos := SynEdit1.ScreenToClient(Mouse.CursorPos);
   Cmd := GetCommandUnderMouse(MousePos.X, MousePos.Y);
   if Cmd <> '' then
     ExecuteUnixCommand(Cmd);
 end;
+
 
 procedure TForm1.ExecuteUnixCommand(const Cmd: string);
 var
